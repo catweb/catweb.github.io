d9eaa2a6f8a27c9073f8be220e011538a607deb0
shumshum <vladimir.pozdyshev@t-agency.ru>
2018-02-08 11:45:55 +0300

    test for rs

---
+++ rs/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+(function () {
+    $('.service__tab').on('click', function (e) {
+        e.preventDefault();
+        $(this).closest('.service').toggleClass('active');
+    });
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOlsiJCIsIm9uIiwiZSIsInByZXZlbnREZWZhdWx0IiwiY2xvc2VzdCIsInRvZ2dsZUNsYXNzIl0sIm1hcHBpbmdzIjoiOztBQUFBLENBQUEsWUFBQTtBQUNBQSxNQUFBLGVBQUEsRUFBQUMsRUFBQSxDQUFBLE9BQUEsRUFBQSxVQUFBQyxDQUFBLEVBQUE7QUFDQUEsVUFBQUMsY0FBQTtBQUNBSCxVQUFBLElBQUEsRUFBQUksT0FBQSxDQUFBLFVBQUEsRUFBQUMsV0FBQSxDQUFBLFFBQUE7QUFDQSxLQUhBO0FBSUEsQ0FMQSIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCl7XHJcbiAgICAkKCcuc2VydmljZV9fdGFiJykub24oJ2NsaWNrJywgZnVuY3Rpb24oZSl7XHJcbiAgICAgICAgZS5wcmV2ZW50RGVmYXVsdCgpO1xyXG4gICAgICAgICQodGhpcykuY2xvc2VzdCgnLnNlcnZpY2UnKS50b2dnbGVDbGFzcygnYWN0aXZlJyk7XHJcbiAgICB9KTtcclxufSkoKTsiXX0=
